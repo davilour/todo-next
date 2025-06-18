b6ed2abce3ed3e9933219a5ac73c583965b9466f
davilour <davi.lourenco1702@gmail.com>
2025-06-18 18:09:01 -0300

    feat(ui): add navigation menu, card and button components

---
+++ components/NavBar.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { NavigationMenu, NavigationMenuList, NavigationMenuItem } from '@/components/ui/navigation-menu';
+
+export function Navbar () {
+    return(
+        <NavigationMenu className='flex justify-center p-4 w-5xl'>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <Link href="/">
+                    <Button>Todo List</Button>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/posts">
+                    <Button>Posts</Button>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/blog">
+                    <Button>Blog</Button>
+                    </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
