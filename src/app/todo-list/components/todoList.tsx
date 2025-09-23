38f2de4b836259d2512910f0b5837d241e699a59
davilour <davi.lourenco1702@gmail.com>
2025-09-22 22:43:15 -0300

    feat(todo-list): change todo list layout to horizontal
    ci: add discord notification workflow for dev branch

--- src/app/todo-list/components/todoList.tsx
+++ src/app/todo-list/components/todoList.tsx
@@ -10,7 +10,7 @@ export function TodoList () {
   if(!todos) return null;
   
   return (
-    <div className="flex flex-col items-center gap-5">
+    <div className="flex flex-row items-center gap-2">
       {todos.map((todo) => (
         <TodoItem key={todo.id} id={todo.id} content={todo.content} isPending={todo.isPending} />
       ))}
